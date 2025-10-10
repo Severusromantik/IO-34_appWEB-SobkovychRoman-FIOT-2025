63a64c50111c5723347f360f9b6a5dda0f48c1df
Severusromantik <romantukepohu@gmail.com>
2025-10-10 09:36:43 +0300

    web_2.0_commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const optionMenu = document.querySelector(".select-menu"),
+       selectBtn = optionMenu.querySelector(".select-button"),
+       options = optionMenu.querySelectorAll(".option"),
+       sLngBtn_text = optionMenu.querySelector(".sLngBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sLngBtn_text.innerText = selectedOption;
+        optionMenu.classList.remove("active");
+    });
+});
